aa9a99ada8b0d4221406f0b49e891eb92fc9fe8f
Timo Reitz <godsboss@gmx.de>
2011-08-21 17:01:45 +0200

    Added mutable collection with empty data set.

--- spec/Collection.js
+++ spec/Collection.js
@@ -8,8 +8,17 @@ describe("Mutable collection", function(){
 
 		it("has a length of zero.", function(){
 			var collection = new Collection.Mutable();
-			expect(collection.length()).toEqual(0);});
-	});
+			expect(collection.length()).toEqual(0);});});
+
+	describe("created with an empty data set", function(){
+
+		it("is empty.", function(){
+			var collection = new Collection.Mutable([]);
+			expect(collection.isEmpty()).toBeTruthy();});
+
+		it("has a length of zero.", function(){
+			var collection = new Collection.Mutable([]);
+			expect(collection.length()).toEqual(0);});});
 
 });
 
