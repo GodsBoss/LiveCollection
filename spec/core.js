5b15681d7f6602d5aeba71484d4dafeaa43fc994
Timo Reitz <godsboss@gmx.de>
2011-08-21 23:55:16 +0200

    Fixed a typo.

--- spec/core.js
+++ spec/core.js
@@ -3,7 +3,7 @@ describe("Live collection", function(){
 	it("can be instantiated directly.", function(){
 		var collection = new LiveCollection.MutableCollection([]);});
 
-	it("containts the values within the data set.", function(){
+	it("contains the values within the data set.", function(){
 		var data = [1, -2, 5];
 		var collection = new LiveCollection.MutableCollection(data);
 		expect(collection.values()).toEqual(data);});
